cc28512846e783801ae0bc6570c47218c3f00643
Tatiana <tatiana.slonimskaia@northgravity.com>
2020-06-29 18:01:50 +0200

    Created main class to start application.

--- src/main/java/com/luxoft/luxofttecnhicaltask/UploadingFilesApplication.java
+++ src/main/java/com/luxoft/luxofttecnhicaltask/UploadingFilesApplication.java
@@ -1,4 +1,13 @@
 package com.luxoft.luxofttecnhicaltask;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class UploadingFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadingFilesApplication.class, args);
+    }
 }
