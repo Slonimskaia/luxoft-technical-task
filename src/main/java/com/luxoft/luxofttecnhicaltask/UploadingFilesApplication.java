4c9a8be25ec8036fb5afd59fe7595f7657a77009
Tatiana <tatiana.slonimskaia@northgravity.com>
2020-07-04 16:51:22 +0200

    Added data base into application.

--- src/main/java/com/luxoft/luxofttecnhicaltask/UploadingFilesApplication.java
+++ src/main/java/com/luxoft/luxofttecnhicaltask/UploadingFilesApplication.java
@@ -2,22 +2,29 @@ package com.luxoft.luxofttecnhicaltask;
 
 
 import com.luxoft.luxofttecnhicaltask.service.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class UploadingFilesApplication {
+public class UploadingFilesApplication implements CommandLineRunner{
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    FileStorageService storageService;
 
     public static void main(String[] args) {
         SpringApplication.run(UploadingFilesApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner init(FileStorageService storageService) {
-        return (args) -> {
-            storageService.init();
-        };
+
+    @Override
+    public void run(String... args) throws Exception {
+        storageService.init();
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS ITEMS (FILE_NAME TEXT, PRIMARY_KEY TEXT, NAME TEXT,DESCRIPTION TEXT, UPDATED_TIMESTAMP TEXT, CONSTRAINT UNIQUE_FILE_NAME_AND_PRIMARY_KEY_COLUMN UNIQUE(FILE_NAME, PRIMARY_KEY));");
     }
 }
