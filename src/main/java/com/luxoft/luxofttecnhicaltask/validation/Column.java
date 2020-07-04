96d31f46dce3f731975b27f9c7df17abe83dbb94
Tatiana <tatiana.slonimskaia@northgravity.com>
2020-07-04 23:02:02 +0200

    Added index for item columns.

--- src/main/java/com/luxoft/luxofttecnhicaltask/validation/Column.java
+++ src/main/java/com/luxoft/luxofttecnhicaltask/validation/Column.java
@@ -1,8 +1,18 @@
 package com.luxoft.luxofttecnhicaltask.validation;
 
 public enum Column {
-    PRIMARY_KEY,
-    NAME,
-    DESCRIPTION,
-    UPDATED_TIMESTAMP
+    PRIMARY_KEY(0),
+    NAME(1),
+    DESCRIPTION(2),
+    UPDATED_TIMESTAMP(3);
+
+    private final int column;
+
+    Column(int column) {
+        this.column = column;
+    }
+
+    public int getColumn() {
+        return column;
+    }
 }
