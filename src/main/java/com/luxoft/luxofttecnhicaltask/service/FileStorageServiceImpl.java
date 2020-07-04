f14ded7c35865334ac0d693900e61feb5d909939
Tatiana <tatiana.slonimskaia@northgravity.com>
2020-07-04 23:03:51 +0200

    Implemented all rows adding from uploaded file into db.

--- src/main/java/com/luxoft/luxofttecnhicaltask/service/FileStorageServiceImpl.java
+++ src/main/java/com/luxoft/luxofttecnhicaltask/service/FileStorageServiceImpl.java
@@ -1,9 +1,12 @@
 package com.luxoft.luxofttecnhicaltask.service;
 
+import com.luxoft.luxofttecnhicaltask.model.CsvFile;
+import com.luxoft.luxofttecnhicaltask.model.Item;
 import com.luxoft.luxofttecnhicaltask.storage.StorageProperties;
 import com.luxoft.luxofttecnhicaltask.storage.exception.StorageException;
 import com.luxoft.luxofttecnhicaltask.storage.exception.StorageFileNotFoundException;
 import com.luxoft.luxofttecnhicaltask.validation.FileValidator;
+import com.opencsv.CSVReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -16,9 +19,10 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.util.List;
 import java.util.stream.Stream;
 
+import static com.luxoft.luxofttecnhicaltask.validation.Column.*;
 import static org.springframework.util.StringUtils.cleanPath;
 
 @Service
@@ -26,6 +30,12 @@ public class FileStorageServiceImpl implements FileStorageService {
 
     private final Path rootLocation;
     private FileValidator validator;
+
+    @Autowired
+    private FileService fileService;
+
+    @Autowired
+    private ItemService itemService;
 
     @Autowired
     public FileStorageServiceImpl(StorageProperties properties) {
@@ -40,9 +50,14 @@ public class FileStorageServiceImpl implements FileStorageService {
             if (!isFileValid(file)) {
                 throw new IOException();
             }
-            try (InputStream inputStream = file.getInputStream()) {
-                Files.copy(inputStream, this.rootLocation.resolve(filename),
-                        StandardCopyOption.REPLACE_EXISTING);
+            try (InputStream inputStream = file.getInputStream();
+                 Reader reader = new InputStreamReader(inputStream, Charset.defaultCharset());
+                 CSVReader csvReader = new CSVReader((reader))) {
+                CsvFile csvFile = new CsvFile(filename);
+                fileService.add(csvFile);
+                List<String[]> allLines = csvReader.readAll();
+                int firstFileRow = 1;
+                allLines.subList(firstFileRow, allLines.size()).forEach(i -> itemService.add(getItem(i, csvFile)));
             }
         } catch (IOException e) {
             throw new SecurityException("Failed to store file " + filename, e);
@@ -85,19 +100,19 @@ public class FileStorageServiceImpl implements FileStorageService {
         }
     }
 
-    @Override
-    public void init() {
-        try {
-            Files.createDirectories(rootLocation);
-        }
-        catch (IOException e) {
-            throw new StorageException("Could not initialize storage", e);
-        }
-    }
-
     private boolean isFileValid(MultipartFile file) throws IOException {
         return validator.lastLineIsEmpty(file, Charset.defaultCharset()) &&
                 validator.documentHasValidLines(file, Charset.defaultCharset()) &&
                 validator.fileHasValidHeader(file, Charset.defaultCharset());
+    }
+
+    private Item getItem(String[] itemField, CsvFile csvFile){
+        Item item = new Item();
+        item.setCsvFileId(csvFile);
+        item.setPrimaryKey(itemField[PRIMARY_KEY.getColumn()]);
+        item.setName(itemField[NAME.getColumn()]);
+        item.setDescription(itemField[DESCRIPTION.getColumn()]);
+        item.setUpdatedTimestamp(itemField[UPDATED_TIMESTAMP.getColumn()]);
+        return item;
     }
 }
