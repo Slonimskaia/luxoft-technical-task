e8b9f143f4bf2e81cdf23f7e0b0f709c3a164ecd
Tatiana <tatiana.slonimskaia@northgravity.com>
2020-07-04 19:15:16 +0200

    Implemented adding and deletion items from db.

---
+++ src/main/java/com/luxoft/luxofttecnhicaltask/service/ItemService.java
@@ -0,0 +1,10 @@
+package com.luxoft.luxofttecnhicaltask.service;
+
+import com.luxoft.luxofttecnhicaltask.model.Item;
+
+public interface ItemService {
+
+    void add(Item item);
+
+    void delete(Item item);
+}
---
+++ src/main/java/com/luxoft/luxofttecnhicaltask/service/ItemServiceImpl.java
@@ -0,0 +1,23 @@
+package com.luxoft.luxofttecnhicaltask.service;
+
+import com.luxoft.luxofttecnhicaltask.dao.ItemRepository;
+import com.luxoft.luxofttecnhicaltask.model.Item;
+
+public class ItemServiceImpl implements ItemService {
+
+    private final ItemRepository repository;
+
+    public ItemServiceImpl(ItemRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void add(Item item) {
+        repository.save(item);
+    }
+
+    @Override
+    public void delete(Item item) {
+        repository.delete(item);
+    }
+}
