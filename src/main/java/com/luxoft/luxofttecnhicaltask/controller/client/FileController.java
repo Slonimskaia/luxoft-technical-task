d36f9b8b9bfd433d9fd9329543bc81fed38cb58a
Tatiana <tatiana.slonimskaia@northgravity.com>
2020-07-05 17:11:05 +0200

    Showed uploaded file content.

--- src/main/java/com/luxoft/luxofttecnhicaltask/controller/client/FileController.java
+++ src/main/java/com/luxoft/luxofttecnhicaltask/controller/client/FileController.java
@@ -4,10 +4,7 @@ import com.luxoft.luxofttecnhicaltask.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -36,5 +33,12 @@ public class FileController {
         redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + "!");
 
         return "redirect:/user";
+    }
+
+    @GetMapping("/files/{fileName}")
+    public String getFileContent(@PathVariable String fileName, Model model) {
+
+        model.addAttribute("items", storageService.getCsvFile(fileName));
+        return "fileContent";
     }
 }
