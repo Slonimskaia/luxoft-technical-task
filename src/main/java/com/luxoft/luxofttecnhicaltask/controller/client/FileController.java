5dce97848e48accfa3711c0f657650c1bdbefdb2
Tatiana <tatiana.slonimskaia@northgravity.com>
2020-07-05 15:37:02 +0200

    Added uploading file into db for user, and also added showing of uploaded files.

--- src/main/java/com/luxoft/luxofttecnhicaltask/controller/client/FileController.java
+++ src/main/java/com/luxoft/luxofttecnhicaltask/controller/client/FileController.java
@@ -5,7 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping(value = "/user")
@@ -23,5 +27,14 @@ public class FileController {
 
         model.addAttribute("files", storageService.getAllFilesNames());
         return "userUploadingFileForm";
+    }
+
+    @PostMapping
+    public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/user";
     }
 }
