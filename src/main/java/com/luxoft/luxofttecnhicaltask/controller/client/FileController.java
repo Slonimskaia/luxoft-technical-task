51daa721276e3b2e0cd320c7d346997168fc4c68
Tatiana <tatiana.slonimskaia@northgravity.com>
2020-07-07 00:23:57 +0200

    Informed user about not found item.

--- src/main/java/com/luxoft/luxofttecnhicaltask/controller/client/FileController.java
+++ src/main/java/com/luxoft/luxofttecnhicaltask/controller/client/FileController.java
@@ -1,5 +1,6 @@
 package com.luxoft.luxofttecnhicaltask.controller.client;
 
+import com.luxoft.luxofttecnhicaltask.model.Item;
 import com.luxoft.luxofttecnhicaltask.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import static java.util.Objects.isNull;
 
 @Controller
 @RequestMapping(value = "/user")
@@ -45,7 +48,12 @@ public class FileController {
     @GetMapping("/files/{fileName}/{primaryKey}")
     public String getItemByPrimaryKey(@RequestParam("primary_key") String primaryKey, @PathVariable String fileName, Model model) {
 
-        model.addAttribute("item", storageService.getByPrimaryKey(primaryKey, fileName));
+        Item item = storageService.getByPrimaryKey(primaryKey, fileName);
+        if (isNull(item)) {
+            model.addAttribute("itemNotFound", primaryKey);
+        } else {
+            model.addAttribute("item", storageService.getByPrimaryKey(primaryKey, fileName));
+        }
         model.addAttribute("items", storageService.getCsvFile(fileName));
         model.addAttribute("fileName", fileName);
         return "fileContent";
