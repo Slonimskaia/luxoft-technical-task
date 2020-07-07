6e0a127fa64e58bfb5a869f2c1eabda9c86c7822
Tatiana <tatiana.slonimskaia@northgravity.com>
2020-07-07 18:24:14 +0200

    Showed content of uploaded file for admin.

--- src/main/java/com/luxoft/luxofttecnhicaltask/controller/admin/AdminFileController.java
+++ src/main/java/com/luxoft/luxofttecnhicaltask/controller/admin/AdminFileController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,5 +24,12 @@ public class AdminFileController {
 
         model.addAttribute("files", storageService.getAllFilesNames());
         return "listOfFiles";
+    }
+
+    @GetMapping("/files/{fileName}")
+    public String getFileContent(@PathVariable String fileName, Model model) {
+
+        model.addAttribute("items", storageService.getCsvFile(fileName));
+        return "adminFileContent";
     }
 }
